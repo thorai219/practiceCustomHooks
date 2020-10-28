480ef1298675060d1e4af23f883ca4880938e97d
Terry Lee <slsk0219@gmail.com>
2020-10-27 18:16:53 -0700

    modularize code with custom hooks

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFlip } from './hooks'
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [faceUp, flip] = useFlip()
   return (
     <img
-      src={isFacingUp ? front : back}
+      src={faceUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={flip}
       className="PlayingCard Card"
     />
   );
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const useFlip = () => {
   const [faceUp, setFaceUp] = useState(true)
@@ -10,4 +11,33 @@ const useFlip = () => {
   return [faceUp, flip]
 }
 
-export default useFlip;
+function useAxios(keyInLocalStorage, baseUrl) {
+  const [res, setRes] = useLocalStorage(keyInLocalStorage)
+
+  const addResData = async (formatter = data => data, pokemonName = '') => {
+    const res = await axios.get(`${baseUrl}${pokemonName}`)
+    setRes(data => [...data, formatter(res.data)])
+  }
+
+  const clearRes = () => setRes([])
+
+  return [res, addResData, clearRes]
+}
+
+function useLocalStorage(key, initVal = []) {
+  if (localStorage.getItem(key)) {
+    initVal = JSON.parse(localStorage.getItem(key))
+  }
+
+  const [val, setVal] = useState(initVal)
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(val))
+  }, [val, key])
+
+  return [val, setVal]
+}
+
+export default useLocalStorage
+
+export { useFlip, useAxios, useLocalStorage }
